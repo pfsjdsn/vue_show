9ab1d4ee4a799a7e5ad5980c19f5dd83c5075c9c
pfsjdsn <canoecanoe@aliyun.com>
2021-11-28 10:19:12 +0800

    18 登录退出-实现表单的数据绑定

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,14 +6,21 @@
         <img src="../assets/logo.png" alt="" />
       </div>
       <!-- 登录表单区 -->
-      <el-form class="login_form">
+      <el-form class="login_form" v-model="loginForm">
         <!-- 用户名 -->
         <el-form-item>
-          <el-input prefix-icon="iconfont icon-user"></el-input>
+          <el-input
+            prefix-icon="iconfont icon-user"
+            v-model="loginForm.username"
+          ></el-input>
         </el-form-item>
         <!-- 密码 -->
         <el-form-item>
-          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+          <el-input
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+          ></el-input>
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
@@ -26,7 +33,14 @@
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        username: 'zs',
+        password: '123'
+      }
+    }
+  }
 }
 </script>
 <style  lang="less" scoped>
