5706007eac1e28db562ae7b9500185be61a704ef
pfsjdsn <canoecanoe@aliyun.com>
2021-11-28 09:57:59 +0800

    16 登录退出-绘制登录表单区域

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
-
+import { Button, Form, FormItem, Input } from 'element-ui'
 Vue.config.productionTip = false
-
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 new Vue({
   router,
   render: h => h(App)
