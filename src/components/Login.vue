a33553819c56ea954482152a56821af3c8962098
pfsjdsn <canoecanoe@aliyun.com>
2021-11-28 21:09:06 +0800

    21 登录退出-实现登录前表单数据的预验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -29,7 +29,7 @@
         </el-form-item>
         <!-- 按钮区域 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
           <el-button type="info" @click="restLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -65,6 +65,12 @@ export default {
     // 重置表单
     restLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    // 登录
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        console.log(valid, 'valid')
+      })
     }
   }
 }
