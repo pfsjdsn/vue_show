f799f220cf8c222616bdc173f87e968db330bf5d
pfsjdsn <canoecanoe@aliyun.com>
2021-12-05 16:29:24 +0800

    26 登录退出-实现退出功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Home组件</div>
+  <div>
+    <el-button type="info" @click="logOut">退出</el-button>
+  </div>
 </template>
 <script>
 export default {
-
+  methods: {
+    //   退出
+    logOut () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 <style lange="less" scoped>
