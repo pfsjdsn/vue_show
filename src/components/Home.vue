92ba7793b1b44e8660093dcf0e95e047a400d1f8
pfsjdsn <canoecanoe@aliyun.com>
2022-01-01 09:36:22 +0800

    30 主页-实现基本的主页布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <el-button type="info" @click="logOut">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部 -->
+    <el-header
+      >Header<el-button type="info" @click="logOut">退出</el-button></el-header
+    >
+    <!-- 页面主体 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">Aside</el-aside>
+      <!-- 右侧内容主体 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
@@ -15,4 +25,16 @@ export default {
 }
 </script>
 <style lange="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d3f;
+}
+.el-aside {
+  background-color: #323743;
+}
+.el-main {
+  background-color: #e9edf0;
+}
 </style>
