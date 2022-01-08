27877b621f69c3a43a77ad79726ff3ce088e305d
pfsjdsn <canoecanoe@aliyun.com>
2022-01-08 16:56:55 +0800

    35 主页-通过双层for循环渲染左侧菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,18 +19,27 @@
           active-text-color="#ffd04b"
         >
           <!-- 一级菜单 -->
-          <el-submenu index="1">
+          <!-- 把id变成字符串形式 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
             <!-- 一级菜单的模板区域 -->
             <template slot="title">
               <!-- 图标 -->
               <i class="el-icon-location"></i>
               <!-- 文本 -->
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
-            <el-menu-item index="1-4-1">
+            <el-menu-item
+              :index="subItem.id + ''"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+            >
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>{{ subItem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
