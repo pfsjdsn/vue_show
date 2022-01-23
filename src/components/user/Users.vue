47f8c644a427053cfecad69aeda40da339ddaba5
pfsjdsn <canoecanoe@aliyun.com>
2022-01-23 17:38:30 +0800

    53 添加用户-渲染添加用户的表单

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -91,9 +91,27 @@
       >
       </el-pagination>
       <!-- 添加用户的对话框 -->
-      <el-dialog title="提示" :visible.sync="addDialogVisible" width="50%">
+      <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
         <!-- 内容主体区域 -->
-        <span>这是一段信息</span>
+        <el-form
+          :model="addForm"
+          :rules="addFormRules"
+          ref="addFormRef"
+          label-width="70px"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="addForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机" prop="mobile">
+            <el-input v-model="addForm.mobile"></el-input>
+          </el-form-item>
+        </el-form>
         <!-- 底部区域 -->
         <span slot="footer" class="dialog-footer">
           <el-button @click="addDialogVisible = false">取 消</el-button>
@@ -108,6 +126,30 @@
 <script>
 export default {
   data () {
+    // 邮箱验证
+    var checkEamil = (rule, value, callback) => {
+      const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!value) {
+        return callback(new Error('请输入邮箱!'))
+      }
+      if (!reg.test(value)) {
+        return callback(new Error('邮箱格式错误！'))
+      } else {
+        callback()
+      }
+    }
+    // 手机验证
+    var checkMobile = (rule, value, callback) => {
+      const reg = /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/
+      if (!value) {
+        return callback(new Error('请输入手机号码!'))
+      }
+      if (!reg.test(value)) {
+        return callback(new Error('手机号码错误！'))
+      } else {
+        callback()
+      }
+    }
     return {
       // 用户信息列表分页对象
       queryInfo: {
@@ -120,7 +162,31 @@ export default {
       // 总页数
       total: 0,
       // 控制添加用户对话框的显示与隐藏
-      addDialogVisible: false
+      addDialogVisible: false,
+      // 添加用户的表单数据
+      addForm: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
+      // 添加表单的验证规则对象
+      addFormRules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '用户名的长度在3~10个字符之间', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '用户名的长度在6~15个字符之间', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, validator: checkEamil, trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, validator: checkMobile, trigger: 'blur' }
+        ]
+      }
     }
   },
   created () {
