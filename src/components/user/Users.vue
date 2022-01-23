0b32a8043016bb15766f9c66ccac8a9dc2e241b1
pfsjdsn <canoecanoe@aliyun.com>
2022-01-23 09:33:07 +0800

    51 用户列表-实现搜索功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 用户信息页 -->
+  <!-- 用户信息页 -->
   <div>
     <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
@@ -12,8 +12,17 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input
+            placeholder="请输入内容"
+            v-model="queryInfo.query"
+            clearable
+            @clear="getUserList"
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getUserList"
+            ></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
