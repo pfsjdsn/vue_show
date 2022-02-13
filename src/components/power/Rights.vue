a00e644736bc2a392be2a76b4fabeaca745432e1
pfsjdsn <canoecanoe@aliyun.com>
2022-02-13 11:17:49 +0800

    71 权限列表-渲染权限列表UI结构

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -3,11 +3,26 @@
     <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>权限管理</el-breadcrumb-item></el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图 -->
-    <el-card>2333 </el-card>
+    <el-card>
+      <el-table :data="rightsList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column prop="authName" label="权限名称"> </el-table-column>
+        <el-table-column prop="path" label="路径"> </el-table-column>
+        <el-table-column prop="level" label="权限等级">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.level === '0'">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level === '1'"
+              >二级</el-tag
+            >
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 <script>
@@ -31,7 +46,6 @@ export default {
         return this.$message.error('获取权限列表失败！')
       }
       this.rightsList = res.data
-      console.log(this.rightsList, ' this.rightsList');
     }
   }
 
