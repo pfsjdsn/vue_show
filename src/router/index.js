53d5b5d0495487db494e24dcb4cbbbccc7f4103a
pfsjdsn <canoecanoe@aliyun.com>
2022-02-13 10:27:39 +0800

    68 权限列表-通过路由展示权限列表组件

--- src/router/index.js
+++ src/router/index.js
@@ -7,32 +7,37 @@ import Home from '@/components/Home'
 import Welcome from '@/components/Welcome'
 // 用户信息页
 import Users from '@/components/user/Users'
+import Rights from '@/components/power/Rights'
 
 Vue.use(Router)
 
 const router = new Router({
   routes: [{
-    path: '/',
-    redirect: '/login'
-  },
-  {
-    path: '/login',
-    component: Login
-  },
-  {
-    path: '/home',
-    component: Home,
-    redirect: '/welcome',
-    children: [{
-      path: '/welcome',
-      component: Welcome
+      path: '/',
+      redirect: '/login'
     },
     {
-      path: '/users',
-      component: Users
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [{
+          path: '/welcome',
+          component: Welcome
+        },
+        {
+          path: '/users',
+          component: Users
+        },
+        {
+          path: '/rights',
+          component: Rights
+        }
+      ]
     }
-    ]
-  }
   ]
 })
 
