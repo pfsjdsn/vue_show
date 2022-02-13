090a7794445d85f04e3f33f798f81a6d21cc9a8a
pfsjdsn <canoecanoe@aliyun.com>
2022-02-13 11:01:19 +0800

    70 权限列表-调用API获取权限列表的数据

--- src/components/power/Rights.vue
+++ src/components/power/Rights.vue
@@ -3,8 +3,8 @@
     <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item></el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>2333 </el-card>
@@ -12,6 +12,28 @@
 </template>
 <script>
 export default {
+  data () {
+    return {
+      // 权限列表
+      rightsList: []
+
+    }
+  },
+  created () {
+    //   获取所有的权限
+    this.getRightsList()
+  },
+  methods: {
+    //获取权限列表
+    async getRightsList () {
+      const { data: res } = await this.$http.get('rights/list')
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取权限列表失败！')
+      }
+      this.rightsList = res.data
+      console.log(this.rightsList, ' this.rightsList');
+    }
+  }
 
 }
 </script>
