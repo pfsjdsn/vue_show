4dd60c70389a8931c52ec773588daa89247efe8c
pfsjdsn <canoecanoe@aliyun.com>
2022-03-02 15:30:00 +0800

    79 角色列表-美化一级权限的UI结构

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -18,10 +18,15 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template slot-scope="scope">
-            <el-row v-for="item1 in scope.row.children" :key="item1.id">
+            <el-row
+              :class="['bdbottom', i1 === 0 ? 'bdtop' : '']"
+              v-for="(item1, i1) in scope.row.children"
+              :key="item1.id"
+            >
               <!-- 渲染一级权限 -->
               <el-col :span="5">
                 <el-tag>{{ item1.authName }}</el-tag>
+                <i class="el-icon-caret-right"></i>
               </el-col>
               <!-- 渲染二级和三级权限 -->
               <el-col :span="19"></el-col>
@@ -73,4 +78,13 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
 </style>
