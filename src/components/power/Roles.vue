95e50b6b1776192bc41edab1f224ff70a22c541a
pfsjdsn <canoecanoe@aliyun.com>
2022-03-13 09:05:51 +0800

    87 分配权限-优化树形控件的展示效果

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -97,7 +97,13 @@
       width="50%"
     >
       <!-- 树形控件 -->
-      <el-tree :data="rightsList" :props="treeProps"></el-tree>
+      <el-tree
+        :data="rightsList"
+        :props="treeProps"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+      ></el-tree>
       <span slot="footer" class="dialog-footer">
         <el-button @click="setRightDialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="setRightDialogVisible = false"
