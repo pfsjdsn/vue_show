2ac3bddb4f9c11f7d26ee2f647e8dfa5c0727a0b
pfsjdsn <canoecanoe@aliyun.com>
2022-03-14 15:32:11 +0800

    99 商品分类-绘制商品分类组件的基本页面布局

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>商品分类组件</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图区域 -->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+      <!-- 表格 -->
+      <!-- 分页区域 -->
+    </el-card>
+  </div>
 </template>
 <script>
 export default {
