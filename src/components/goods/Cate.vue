973fe8a31a0f4118a52dd93235c64a10af353bf8
pfsjdsn <canoecanoe@aliyun.com>
2022-03-14 16:22:27 +0800

    102 商品分类-使用自定义模板列渲染表格数据

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -23,7 +23,16 @@
         index-text="#"
         border
         :show-row-hover="false"
-      ></tree-table>
+      >
+        <template slot="isok" slot-scope="scope">
+          <i
+            class="el-icon-success"
+            v-if="scope.row.cat_deleted === false"
+            style="color: lightgreen"
+          ></i>
+          <i class="el-icon-error" v-else style="color: red"></i>
+        </template>
+      </tree-table>
       <!-- 分页区域 -->
     </el-card>
   </div>
@@ -47,6 +56,13 @@ export default {
         {
           label: '分类名称',
           prop: 'cat_name'
+        },
+        {
+          label: '是否有效',
+          // 模板列
+          type: 'template',
+          // 模板名称
+          template: 'isok'
         }
       ]
     }
