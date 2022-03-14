ecd4cebc308d2677edf15970d0db4f94ee719f01
pfsjdsn <canoecanoe@aliyun.com>
2022-03-14 15:26:36 +0800

    98 商品分类-通过路由加载商品分类组件

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Welcome from '@/components/Welcome'
 import Users from '@/components/user/Users'
 import Rights from '@/components/power/Rights'
 import Roles from '@/components/power/Roles'
+import Cate from '@/components/goods/Cate'
 
 Vue.use(Router)
 
@@ -42,6 +43,10 @@ const router = new Router({
         {
           path: '/roles',
           component: Roles
+        },
+        {
+          path: '/categories',
+          component: Cate
         }
       ]
     }
